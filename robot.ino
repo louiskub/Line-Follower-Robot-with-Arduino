1e2ff51885ba826387f6a9f5da485d22c934182e
Manatsavin Kunnantajiam <louismnsv@gmail.com>
2023-10-25 02:36:56 +0700

    Update robot.ino

--- robot.ino
+++ robot.ino
@@ -4,16 +4,21 @@ const int in1 = 9;
 const int in2 = 8;
 const int in3 = 10;
 const int in4 = 11;
-
-const int VELA = 95;
+const int photo = A0;
+const int VELA = 110;
 const int VELB = VELA+10;
+const int tilt = 20;
 
-const int tilt = 5;
-const int normal = 400;
-const int forwardNormal = 200;
+bool turn = 0;
+int foundDark = 1;
+unsigned int waitTime;
+unsigned int whiteTimer;
 
-const int obtuse = 680;
-const int forwardObtuse = 500;
+int intersecttimer;
+int whiteinterval = 600;
+
+bool flashdetected = false;
+unsigned long flashstarttime = 0;
 
 struct threshold{
   int dark,light,mid;
@@ -28,30 +33,22 @@ void stop(int duration){
 }
 void left(int duration){
   digitalWrite(in1,0);
-  digitalWrite(in2,0);
-  digitalWrite(in3,0);
-  digitalWrite(in4,0);
-
-  digitalWrite(in1,0);
   digitalWrite(in2,1);
   digitalWrite(in3,1);
   digitalWrite(in4,0);
-  
+  analogWrite(enA,100);
+  analogWrite(enB,110);
   delay(duration);
 
   stop(0);
 }
 void right(int duration){
-  digitalWrite(in1,0);
-  digitalWrite(in2,0);
-  digitalWrite(in3,0);
-  digitalWrite(in4,0);
-
   digitalWrite(in1,1);
   digitalWrite(in2,0);
   digitalWrite(in3,0);
   digitalWrite(in4,1);
-  
+  analogWrite(enA,100);
+  analogWrite(enB,110);
   delay(duration);
   stop(0);
 }
@@ -60,6 +57,9 @@ void forward(int duration){
   digitalWrite(in2,1);
   digitalWrite(in3,0);
   digitalWrite(in4,1);
+
+  analogWrite(enA,VELA);
+  analogWrite(enB,VELB);
   
   delay(duration);
   stop(0);
@@ -69,17 +69,35 @@ void backward(int duration){
   digitalWrite(in2,0);
   digitalWrite(in3,1);
   digitalWrite(in4,0);
+  analogWrite(enA,VELA);
+  analogWrite(enB,VELB);
   delay(duration);
   stop(0);
 }
 bool checkWhite(){
-  return analogRead(A1) > module[1].mid &&
+  return 
        analogRead(A2) > module[2].mid &&
        analogRead(A3) > module[3].mid &&
        analogRead(A4) > module[4].mid &&
+       analogRead(A1) > module[1].mid &&
        analogRead(A5) > module[5].mid;
 }
-
+bool checkDark(){
+  return 
+       analogRead(A2) <= module[2].mid &&
+       analogRead(A3) <= module[3].mid &&
+       analogRead(A4) <= module[4].mid &&
+       (analogRead(A1) <= module[1].mid ||
+       analogRead(A5) <= module[5].mid);
+}
+bool checkAllDark(){
+  return 
+       analogRead(A2) <= module[2].mid &&
+       analogRead(A3) <= module[3].mid &&
+       analogRead(A4) <= module[4].mid &&
+       analogRead(A1) <= module[1].mid &&
+       analogRead(A5) <= module[5].mid;
+}
 void setup(){
   Serial.begin(9600);
   pinMode(A1,INPUT);
@@ -105,85 +123,96 @@ void setup(){
   module[4].dark = 540;
   module[5].light = 1000;
   module[5].dark = 200;
-  analogWrite(enA,VELA);
-  analogWrite(enB,VELB);
+
   for(int i=1 ; i<=5 ; i++){
     module[i].mid = (module[i].dark + module[i].light)/2;
   }
-
 }
   //Check Dark : <=
   //Check Light : >=
 void loop()
-{  
-  //Mid White
-  while(analogRead(A3) > module[3].mid)
-  {
-    if(analogRead(A1) > module[1].mid &&
-       analogRead(A2) > module[2].mid&&
-       analogRead(A5) > module[5].mid &&
-       analogRead(A4) > module[4].mid)
-    {
-      while(!(analogRead(A3) <= module[3].mid && (analogRead(A2) <= module[2].mid || analogRead(A4) <= module[4].mid)))
-        forward(10);
+{
+  if((checkDark() && foundDark%2 == 0) || (checkAllDark() && foundDark%2 == 1)){
+    foundDark++;
+    while(analogRead(photo) > 300);
+    waitTime = millis();
+    while(millis()-waitTime < 3000)
+      if(analogRead(photo) > 300)
+        while(millis()-waitTime < 3000)
+          if(analogRead(photo) <= 300)
+            turn = true;
+    if(turn){
+      forward(300);
+      right(350);
+      while(analogRead(A3) > module[3].mid && analogRead(A2) > module[2].mid){
+        right(tilt);
+      }
     }
-    //Tilt Left
-    if(analogRead(A1) <= module[1].mid ||
-       analogRead(A2) <= module[2].mid)
-    {
-      left(tilt); 
+    else{
+      forward(300);
+      left(350);
+      while(analogRead(A3) > module[3].mid && analogRead(A4) > module[4].mid){
+        left(tilt);
+      }
     }
-    else if(analogRead(A4) <= module[4].mid || 
-            analogRead(A5) <= module[5].mid)
-    {
-      right(tilt); 
-    }
+    turn = false;
+    whiteTimer = millis();
   }
   
-  //Mid Dark
-  while(analogRead(A3) <= module[3].mid)
-  {
-    if(analogRead(A1) <= module[1].mid &&
-       analogRead(A2) <= module[2].mid &&
-       analogRead(A3) <= module[3].mid &&
-       analogRead(A4) <= module[4].mid)
-    {
-      while(!checkWhite())
-        forward(tilt);
-      right(normal); 
-      forward(forwardNormal); 
+  if(analogRead(A1) <= module[1].mid || analogRead(A2) <= module[2].mid || analogRead(A3) <= module[3].mid || analogRead(A4) <= module[4].mid || analogRead(A5) <= module[5].mid ){
+    if(millis() - whiteTimer >= whiteinterval){
+      forward(50);
+      
     }
+    whiteTimer = millis();
+  }
     
-    //90 Degree Left
-    if(analogRead(A1) <= module[1].mid &&
-       		analogRead(A2) <= module[2].mid)
-    {
-      while(!checkWhite())
-        forward(tilt);
-      right(normal); 
-      forward(forwardNormal); 
+  if(checkWhite() && millis() - whiteTimer >= whiteinterval){
+    backward(100);
+  }
+  
+  else 
+  {
+    if(analogRead(A3) <= module[3].mid && analogRead(A5) <= module[5].mid){
+      intersecttimer = millis();
+      while(millis() - intersecttimer <= 400 && !((checkDark() && foundDark%2 == 0) || (checkAllDark() && foundDark%2 == 1)))
+        forward(1);
+      while(analogRead(A3) > module[3].mid && analogRead(A2) > module[2].mid){
+        whiteTimer = millis();
+        right(tilt);
+      }
+    }
+    else if(analogRead(A3) <= module[3].mid && analogRead(A1) <= module[1].mid){
+      intersecttimer = millis();
+      while(millis() - intersecttimer <= 400 && !((checkDark() && foundDark%2 == 0) || (checkAllDark() && foundDark%2 == 1)))
+        forward(1);
+      while(analogRead(A3) > module[3].mid && analogRead(A4) > module[4].mid){
+        whiteTimer = millis();
+        left(tilt);
+      }
     }
 
-    //315 Degree Left
-    else if(analogRead(A1) <= module[1].mid)
+    if(analogRead(A2) > module[2].mid && analogRead(A4) > module[4].mid) //Mid Dark
     {
-      while(!checkWhite())
-        forward(tilt);
-      left(obtuse); 
-      forward(forwardObtuse); 
+      forward(tilt);      
     }
-    else if(analogRead(A5) <= module[5].mid)
-    {
-      while(!checkWhite())
-        forward(tilt);
-      right(obtuse); 
-      forward(forwardObtuse); 
+    else if(analogRead(A1) <= module[1].mid){
+      whiteTimer = millis();
+      left(tilt);
     }
-
-    //Forward
-    else
-    {
-      forward(10); //Less time = more frequent uppdate
+    else if(analogRead(A5) <= module[5].mid){
+      whiteTimer = millis();
+      right(tilt);
+    }
+    else if(analogRead(A2) <= module[2].mid){
+      whiteTimer = millis();
+      left(tilt);
+      forward(tilt);
+    }
+    else if(analogRead(A4) <= module[4].mid){
+      whiteTimer = millis();
+      right(tilt);
+      forward(tilt);
     }
   }
 }
